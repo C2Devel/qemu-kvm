98f6ef858f90c2e05947297033b9581f427513fa
Jan Kiszka <jan.kiszka@siemens.com>
2012-08-17 08:58:55 +0200

    qemu-kvm: Clear unused IOCTL fields in kvm_device_intx_set_mask
    Helps valgrind, doesn't cost much (compared to the associated PCI config
    space write). Align variable naming to other helpers at this chance.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- target-i386/kvm.c
+++ target-i386/kvm.c
@@ -2118,11 +2118,12 @@ int kvm_device_intx_assign(KVMState *s, uint32_t dev_id, bool use_host_msi,
 
 int kvm_device_intx_set_mask(KVMState *s, uint32_t dev_id, bool masked)
 {
-    struct kvm_assigned_pci_dev assigned_dev;
+    struct kvm_assigned_pci_dev dev_data = {
+        .assigned_dev_id = dev_id,
+        .flags = masked ? KVM_DEV_ASSIGN_MASK_INTX : 0,
+    };
 
-    assigned_dev.assigned_dev_id = dev_id;
-    assigned_dev.flags = masked ? KVM_DEV_ASSIGN_MASK_INTX : 0;
-    return kvm_vm_ioctl(s, KVM_ASSIGN_SET_INTX_MASK, &assigned_dev);
+    return kvm_vm_ioctl(s, KVM_ASSIGN_SET_INTX_MASK, &dev_data);
 }
 
 static int kvm_deassign_irq_internal(KVMState *s, uint32_t dev_id,
